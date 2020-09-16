6bce809657607c8b226d32db32d7a5f7c5dc2fd2
rublin <clInS1L978je>
2020-09-16 20:35:23 +0300

    create a doctor

--- src/main/java/com/mydoctor/mydoctorbot/TelegramBotConnection.java
+++ src/main/java/com/mydoctor/mydoctorbot/TelegramBotConnection.java
@@ -1,7 +1,10 @@
 package com.mydoctor.mydoctorbot;
 
+import com.mydoctor.mydoctorbot.model.Doctor;
+import com.mydoctor.mydoctorbot.service.DoctorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -13,12 +16,18 @@ public class TelegramBotConnection extends TelegramLongPollingBot {
 
     private final String token;
 
+    @Autowired
+    private DoctorService doctorService;
+
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+            String[] split = text.split(" ");
+            Doctor doctor = doctorService.create(split[0], split[1], split[2]);
             SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
                     .setChatId(update.getMessage().getChatId())
-                    .setText("You said: " + update.getMessage().getText());
+                    .setText("Doctor created: " + doctor);
             try {
                 execute(message); // Call method to send the message
             } catch (TelegramApiException e) {
