1a0bb1371762e2f9bc90bf43bf7a6bd0a7dbfb58
Olenka <helen.imena.ua@gmail.com>
2020-11-01 18:53:03 +0200

    Add RegisterDoctorAskLastNameStepService

--- src/main/java/com/mydoctor/mydoctorbot/service/telegram/RegisterDoctorAskLastNameStepService.java
+++ src/main/java/com/mydoctor/mydoctorbot/service/telegram/RegisterDoctorAskLastNameStepService.java
@@ -2,32 +2,32 @@ package com.mydoctor.mydoctorbot.service.telegram;
 
 import com.mydoctor.mydoctorbot.model.SystemUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import static com.mydoctor.mydoctorbot.service.telegram.Step.REGISTER_DOCTOR_ASK_LAST_NAME;
 import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
-public class RegisterDoctorAskFirstNameStepService implements StepService {
+public class RegisterDoctorAskLastNameStepService implements StepService {
     private final SystemUserService systemUserService;
 
     @Override
     public Step getStep() {
-        return Step.REGISTER_DOCTOR_ASK_FIRST_NAME;
+        return REGISTER_DOCTOR_ASK_LAST_NAME;
     }
 
     @Override
     public SendMessage processStep(Message message, SystemUser systemUser) {
         systemUser.setStep(getStep().getNextStep());
-        systemUser.setFirstName(message.getText());
+        systemUser.setLastName(message.getText());
         systemUserService.update(systemUser);
-
         return new SendMessage()
                 .setChatId(systemUser.getTelegramId())
-                .setText(format("Ok, I saved %s as you first name.\n\n", message.getText()) +
-                        "What is your last name?");
+                .setText(format("Ok, I saved %s as you last name.\n\n", message.getText()) +
+                        "What is your specialty?");
     }
+
 }
